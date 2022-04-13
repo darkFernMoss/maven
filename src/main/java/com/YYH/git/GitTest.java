ef64276861f6a3a9fca275abe55f72c8f9988eda
YYH <YYH@qq.com>
2022-04-13 10:20:45 +0800

    first idea commit

---
+++ src/main/java/com/YYH/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.YYH.git;
+
+/**
+ * @author 袁宇浩
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
